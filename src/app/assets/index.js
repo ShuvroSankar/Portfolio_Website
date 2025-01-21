9b0436a53d705a0a9fd6810d7dcfc77d9097ec7f
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-01-22 00:06:19 +0600

    Hero Section

---
+++ src/app/assets/index.js
@@ -0,0 +1,27 @@
+// Hero
+import InstagramLineIcon from "remixicon-react/InstagramLineIcon"
+import FacebookCircleLineIcon from "remixicon-react/FacebookCircleLineIcon"
+import DribbbleLineIcon from "remixicon-react/DribbbleLineIcon"
+import YoutubeLineIcon from "remixicon-react/YoutubeLineIcon"
+import GithubLineIcon from "remixicon-react/GithubLineIcon"
+
+export const heroIcons = [
+    // eslint-disable-next-line react/jsx-key
+    <InstagramLineIcon/>,
+    // eslint-disable-next-line react/jsx-key
+    <FacebookCircleLineIcon/>,
+    // eslint-disable-next-line react/jsx-key
+    <DribbbleLineIcon/>,
+    // eslint-disable-next-line react/jsx-key
+    <YoutubeLineIcon/>,
+    // eslint-disable-next-line react/jsx-key
+    <GithubLineIcon/>,
+
+
+]
+
+// AboutMe Icons 
+import GithubFillIcon from "remixicon-react/GithubFillIcon"
+import Projector2LineIcon from "remixicon-react/Projector2LineIcon"
+import GroupLineIcon from "remixicon-react/GroupLineIcon"
+import AwardFillIcon from "remixicon-react/AwardFillIcon"
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +12,13 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    screens:{
+      xl: {max:"1400px"},
+      lg: {max:"1150px"},
+      md: {max:"767px"},
+      sm: {max:"639px"},
+
+    }
   },
   plugins: [],
 };
