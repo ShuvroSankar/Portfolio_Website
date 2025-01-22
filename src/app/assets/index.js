ea7e7b08a46ba797b3557d5204db97313a837815
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-01-23 00:21:36 +0600

    About Me

--- src/app/assets/index.js
+++ src/app/assets/index.js
@@ -25,3 +25,36 @@ import GithubFillIcon from "remixicon-react/GithubFillIcon"
 import Projector2LineIcon from "remixicon-react/Projector2LineIcon"
 import GroupLineIcon from "remixicon-react/GroupLineIcon"
 import AwardFillIcon from "remixicon-react/AwardFillIcon"
+
+export const aboutData = [
+    {
+        title: "Github Repos",
+        amount: 384,
+        icon: <GithubFillIcon/>,
+
+    },
+
+    {
+        title: "Successful Projects",
+        amount: 384,
+        icon: <Projector2LineIcon/>,
+
+    },
+    
+]
+
+import DownloadLineIcon from "remixicon-react/DownloadLineIcon"
+import ArrowLeftSFillIcon from "remixicon-react/ArrowLeftSFillIcon"
+
+export const downloadLineIcon = <DownloadLineIcon/>
+export const arrowLeftSFillIcon = <ArrowLeftSFillIcon/>
+
+export const aboutText = `Recent B.Sc. graduate in Electrical and Electronic Engineering from American
+International University Bangladesh, eager to leverage academic knowledge in VLSI
+design and automation. Looking to start a career as a Trainee Engineer in IC Physical
+Design with a focus on power, performance, and area optimization using state-of-theart technologies. Proficient in various programming and scripting languages, with a
+strong foundation in digital electronics and physical design processes.`
+
+// End of About Me
+
+// Skills
