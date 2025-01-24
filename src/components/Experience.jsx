35dc4dab22304f12f4c8c1f2aa4e63ead87958a5
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-01-25 02:03:53 +0600

    E&E update 3

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,46 +3,82 @@
 import Heading from "./sub/Heading"
 import Image from "next/image"
 import { arrowLeftIcon, experienceData } from "@/app/assets"
+import { motion, useScroll, useSpring } from 'framer-motion'
+import { useRef } from "react"
 
 const Experience = () => {
   const date = new Date().getFullYear()
-  return (
-    <div  className="relative py-20 px-96">
-      <Heading text={"Experience & Education"}/>
-      <Image src={"/skills/Education.svg"} alt="Experience Image"  width={400} height={400} className="absolute -top-4 right-96 opacity-70 lg:hidden"  /> 
-      <div className="w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10">
-        {experienceData.map((data,i)=>(
-          <div key={`id-${i}`} className="w-[600px] xl:w-[480px] sm:w-full  px-12 sm:px-0 relative -left-[300px]">
-          <div className="relative flex flex-col gap-y-3 rounded-md border border-red-300 bg-white p-4 tracking-wide sm:text-sm">
-          <h1 className="text-xl sm:text-lg font-light text-gray-700">{data.designation}</h1>
-          <p className="text-gray-800">
-                    <span className="block font-light">Company:</span>
-                    <span className="block pl-2 font-extralight">{data.company}</span>
-          </p>
-          <div className="text-gray-800">
-                    <span className="font-light" >Experience:</span>
-                    <ul className="pl-2">
-                      {data.responsibilities.map((rep,j)=>(
-                        <li key={j} className="my-1 font-extralight">{rep}</li>
-                      ))}
-                        
-                    </ul>
-                    
-                </div>
-                <span className="absolute top-20 left-full text-red-300 aspect-square">{arrowLeftIcon}</span>
- 
-          </div>
-          <div className="w-[13rem] mx-2 absolute top-20   left-full border border-gray-300 rounded-full   text-red-400 font-light">{data.period}</div>
-        </div>
-          
-        ))}
-        
-        <div className="absolute w-1 h-full rounded-full bg-gray-300"></div>
-      </div>
 
+  const containerRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start 95', 'end end']
+  })
+  const scrollY = useSpring(scrollYProgress, { stiffness: 200, damping: 30 })
+
+  return (
+    <div className="relative py-20 px-96">
+      <Heading text={"Experience & Education"} />
+      <Image
+        src={"/skills/Education.svg"}
+        alt="Experience Image"
+        width={400}
+        height={400}
+        className="absolute -top-4 right-96 opacity-70 lg:hidden"
+      />
+      <div ref={containerRef} className="w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10">
+        {experienceData.map((data, i) => (
+          <div
+            key={`id-${i}`}
+            className={`w-[600px] xl:w-[480px] sm:w-full px-12 sm:px-0 relative ${
+              i % 2 === 0 ? '-left-[300px] xl:-left-[240px] lg:-left-0' : 'left-[300px] xl:left-[240px] lg:-left-0'
+            }`}
+          >
+            <motion.div
+              initial={{ opacity: 0, x: i % 2 === 0 ? -80 : 80 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.7, type: 'spring', stiffness: 50 }}
+              className="relative flex flex-col gap-y-3 rounded-md border border-red-300 bg-white p-4 tracking-wide sm:text-sm"
+            >
+              <h1 className="text-xl sm:text-lg font-light text-gray-700">{data.designation}</h1>
+              <p className="text-gray-800">
+                <span className="block font-light">Company:</span>
+                <span className="block pl-2 font-extralight">{data.company}</span>
+              </p>
+              <div className="text-gray-800">
+                <span className="font-light">Experience:</span>
+                <ul className="pl-2">
+                  {data.responsibilities.map((rep, j) => (
+                    <li key={j} className="my-1 font-extralight">{rep}</li>
+                  ))}
+                </ul>
+              </div>
+              <span
+                className={`absolute top-20 left-full text-red-300 aspect-square -translate-y-1/2 ${
+                  i % 2 === 0 ? 'left-full rotate-180' : 'right-full'
+                }`}
+              >
+                {arrowLeftIcon} 
+              </span>
+            </motion.div>
+            <div
+              className={`w-[13rem] absolute top-20 border border-gray-300 rounded-full text-red-400 font-light -translate-y-1/2 z-10 bg-white ${
+                i % 2 === 0 ? 'left-full -translate-x-1/2 lg:left-1/2' : 'right-full -translate-x-1/2 lg:right-1/2'
+              }`}
+            >
+              {data.period}
+            </div>
+          </div>
+        ))}
+        <motion.div
+          initial={{ scaleY: 0 }}
+          style={{ scaleY: scrollY }}
+          className="absolute w-1 h-full rounded-full bg-gray-300"
+        ></motion.div>
+      </div>
     </div>
   )
 }
 
 export default Experience
-
